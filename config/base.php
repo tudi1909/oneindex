ef80d12982ea9d10c2fc898530958c6a9d4c7166
tudi1909 <vip029@me.com>
2018-04-20 11:01:10 +0800

    Update base.php

--- config/base.php
+++ config/base.php
@@ -7,6 +7,6 @@
   'cache_expire_time' => 3600,
   'cache_refresh_time' => 600,
   'root_path' => '?',
-  'refresh_token' => 'AQABAAAAAADX8GCi6Js6SK82TsD2Pb7rVIs5BZwWUKvsLmOQQ3XYMDkryo1ZOaqlxKR0lvI8FmTHKrwaoRAxJ3VgRX__m2lUbZeARWLcaC4Im7bxARwwIqycKXWBWP60NX5hYOI4PfjMkV-hZ4xrQuLc18B-C8jcCWzEamaTzusD8usRpE1hmPUgGZsGpecAx0WDNEn9n9kNlrn425mmlcTmK50LCnieI7_KmN8JR7GKjKA_KiV3a9XKar2h2sUzpguVniz__pb-9m-iTIan5ceTupoJL6d5brqNRsbsYEQy7DIVfcZzUUdt_vnm1Lx6avY1I0PUSi5lJ5Z9AOVVUi73yl2aryp_tPXFK-VdTNsQkJ0TqnKAOKWyL0llCo6tg9emXN-ObPAI2rhVQ5cqOPbKg2UnrUmrzR9iy6C-MSeZAKxdWixGrPuo0aVNriLP4mpCGf4eS_gTOPinj9eFm45NdOl6uKTaqm13csF9jCu9VYG5GaDkHFdlpjMNUOQP8_wi796z_4IH0bICRMt-hfcr8PJAmhHBolfPyHn8Bv2lF1RHg4snPUl2aKbmr46TUgZb1ZlevNvEXUl7tcU2hO7EE6NJKvixf38J-Oykt_cpRBhgrX_Sa-vuJ2ZGv7iLh2Ro8aRrt6ZTGLxq_PAZYBsnh4FyFzVWg1QIE1CsmNdcBj3zaErDoqcPbIVrqCapsMcMFJqHwBuigUOHz5T_n4JA75aApPbp9hnApvrmy5lEKCIoX4xtmoCBRA6BwxAUio9jW1lua9aBeXUuCSrakeExGXmRxeOnWUyeoZohHzOi6HoVzvv7SSAA',
-  'app_url' => 'https://dbgeek-my.sharepoint.com/',
+  'refresh_token' => '-hZ4xrQuLc18B',
+  'app_url' => 'https://test.sharepoint.com/',
 );
